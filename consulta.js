9ca59ea4ec34dd54aaef7fbe696565baf70b668f
jfiorott <91494772+jfiorott@users.noreply.github.com>
2021-10-13 16:37:14 -0300

    Add files via upload

---
+++ consulta.js
@@ -0,0 +1,34 @@
+
+function atualizarPlacar() {
+    var escore1 = localStorage.getItem('placar1');
+    var escore2 = localStorage.getItem('placar2');
+
+    var plc1 = document.getElementById('ptoJog1');
+    var plc2 = document.getElementById('ptoJog2');
+
+    plc1.innerHTML = escore1;
+    plc2.innerHTML = escore2;
+
+//    document.body.innerHTML = 'Placar1 : ' + escore1;
+//    document.body.innerHTML = 'Placar2 : ' + escore2;
+}
+
+function iniciarJogo() {
+    var categ1 = localStorage.getItem('categ');
+    var jog1 = localStorage.getItem('jogad1');
+    var jog2 = localStorage.getItem('jogad2');
+
+    var categ2 = document.getElementById('categoria');
+    var jogad1 = document.getElementById('nomeJog1');
+    var jogad2 = document.getElementById('nomeJog2');
+
+    let categ = 'Categoria: ';
+
+    categ2.innerHTML = categ + categ1;
+    jogad1.innerHTML = jog1;
+    jogad2.innerHTML = jog2;
+
+}
+
+iniciarJogo();
+atualizarPlacar();
