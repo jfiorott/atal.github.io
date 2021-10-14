e2a7becd3aee6feb5cb6729ad584102076fd0dec
jfiorott <91494772+jfiorott@users.noreply.github.com>
2021-10-14 10:08:46 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -26,16 +26,28 @@ function iniciarJogo() {
     var categ1 = localStorage.getItem('categ');
     var jog1 = localStorage.getItem('jogad1');
     var jog2 = localStorage.getItem('jogad2');
+    var v_games11 = localStorage.getItem('gamesA1');
+    var v_games12 = localStorage.getItem('gamesA2');
+    var v_games21 = localStorage.getItem('gamesB1');
+    var v_games22 = localStorage.getItem('gamesB2');
 
     var categ2 = document.getElementById('categoria');
     var jogad1 = document.getElementById('nomeJog1');
     var jogad2 = document.getElementById('nomeJog2');
-
+    var t_games11 = document.getElementById('games11');
+    var t_games12 = document.getElementById('games12');
+    var t_games21 = document.getElementById('games21');
+    var t_games22 = document.getElementById('games22');
+    
     let categ = 'Categoria: ';
 
     categ2.innerHTML = categ + categ1;
     jogad1.innerHTML = jog1;
     jogad2.innerHTML = jog2;
+    t_games11.innerHTML = v_games11;
+    t_games12.innerHTML = v_games12;
+    t_games21.innerHTML = v_games21;
+    t_games22.innerHTML = v_games22;
 }
 
 iniciarJogo();
